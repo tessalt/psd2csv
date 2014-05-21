a9028aecb00ae04895fe9564f79b1217622313bf
Tessa <tessathornton@gmail.com>
2014-05-21 12:38:13 -0400

    filter and sort rows

--- lib/psd2csv.rb
+++ lib/psd2csv.rb
@@ -5,18 +5,37 @@ require 'json'
 
 module Psd2csv
   class Parse
+
     def psd2csv
       file = ARGV[0]
       csv_file = ARGV[1] || "psd.csv"
       psd = PSD.new(file, parse_layer_images: true)        
-      CSV.open(csv_file, "w") do |csv|
-        csv << ["layer name", "layer text", "font size"]
-        psd.tree.descendant_layers.each do |layer|
-          unless layer.text.nil?
-            csv << [layer.name, layer.text[:value], layer.text[:font][:sizes]]
+      pattern = /\*\[([a-zA-Z]\d)/
+      rows = Array.new
+
+      psd.tree.descendant_layers.each do |layer|
+        unless layer.text.nil?
+          matches = /\*\[([a-zA-Z]\d)/.match(layer.name)
+          if matches
+            row = Hash.new
+            row[:index] = matches.to_a[1]
+            row[:text] = layer.text[:value]
+            rows.push(row)
           end
         end
-      end    
+      end
+
+      sorted_rows = rows.sort do |a, b| 
+        a[:index].upcase <=> b[:index].upcase
+      end
+
+      CSV.open(csv_file, "w") do |csv|
+        csv << ["index", "layer text"]
+        sorted_rows.each { |row| csv << [row[:index], row[:text]]}
+        puts "File created: #{csv_file}"
+      end
+
     end
+
   end
 end
